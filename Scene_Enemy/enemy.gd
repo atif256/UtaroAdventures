6468265fc808f6e8e487d66d141efd777414343e
atif256 <atifkunn@gmail.com>
2023-12-08 19:35:27 +0800

    updating the gd code
    fix several error

--- Scene_Enemy/enemy.gd
+++ Scene_Enemy/enemy.gd
@@ -1,13 +1,13 @@
 extends Area2D
 
-func _ready():
-	pass
+#func _ready():
+	#pass
 
-func _process(delta):
+func _process(_delta):
 	#position.x += 0.3
-	rotation_degrees +=1
+	rotation_degrees += 1
 
-func _on_body_entered(main):
+func _on_body_entered(_main):
 	queue_free()
 	#pass
 
--- main.gd
+++ main.gd
@@ -2,12 +2,12 @@ extends Node
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+#func _ready():
+	#pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+#func _process(delta):
 	#$Enemy.rotation_degrees +=0.05
-	pass
+	#pass
 
